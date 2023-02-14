5499d217b9a9cf14e048f642ea288b687dd8e931
Clement <clement.pene@sii.fr>
2023-02-14 11:56:16 +0100

    deleted advanced functions

--- TP 1 - Partie sur PC/programme_shell_avec_arguments.c
+++ TP 1 - Partie sur PC/programme_shell_avec_arguments.c
@@ -4,15 +4,6 @@
 #include <unistd.h>
 /* déclaration des types de base */
 #include <sys/types.h>
-/* constantes relatives aux domaines, types et protocoles */
-#include <sys/socket.h>
-/* constantes et structures propres au domaine UNIX */
-#include <sys/un.h>
-/* constantes et structures propres au domaine INTERNET */
-#include <netinet/in.h>
-/* structures retournées par les fonctions de gestion de la base de
-données du réseau */
-#include <netdb.h>
 /* pour les entrées/sorties */
 #include <stdio.h>
 /* pour la gestion des erreurs */
@@ -29,29 +20,11 @@ void main (int argc, char **argv)
 	int timer1_ms = 0; 
 	int timer2_ms = 0; 
 	int timer3_ms = 0;
-	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
-	int en_boucle = 0;
-	int stop = 0;
+
 	char message[100] = "";
 
-	while ((c = getopt(argc, argv, "t:n:bhm")) != -1) {
+	while ((c = getopt(argc, argv, "t:n:bhms")) != -1) {
 		switch (c) {
-            case 'b':
-				en_boucle = 1;
-				if (nb_message != -1) {
-					printf("Mauvais usage. Option '-h' pour l'aide.\n");
-					exit(1) ;
-				}
-                break;
-
-            case 'n':
-                nb_message = atoi(optarg);
-				if (en_boucle != 0) {
-					printf("Mauvais usage. Option '-h' pour l'aide.\n");
-					exit(1) ;
-				}
-                break;
-
             case 't':
 				timer1_ms = atoi(strtok(optarg, " "));
 				timer2_ms = atoi(strtok(NULL, " "));
@@ -76,19 +49,10 @@ void main (int argc, char **argv)
 		}
 	} 
 
-	if ((strcmp(message, "") == 0) && (stop == 0)) {
+	if (strcmp(message, "") == 0) {
 		printf("Précisez un message '-m' ou envoyez la commande stop '-s'. Option '-h' pour l'aide.\n");
 		exit(0);
 	}
 
-
-	if (nb_message != -1) {
-		printf("Message envoyé %d fois.\n", nb_message);
-	} else {
-		if (en_boucle == 1) {
-			printf("Message envoyé en boucle.\n");
-		}
-		printf("Message envoyé 1 fois.\n");
-	}
 }
 
---
+++ TP 1 - Partie sur PC/uart_example.c
@@ -0,0 +1,78 @@
+// C library headers
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+
+
+int main() {
+
+    int serial_port = open("/dev/ttyACM0", O_RDWR);
+
+    // Check for errors
+    if (serial_port < 0) {
+        printf("Error %i from open: %s\n", errno, strerror(errno));
+    }
+
+    // Create new termios struct, we call it 'tty' for convention
+    // No need for "= {0}" at the end as we'll immediately write the existing
+    // config to this struct
+    struct termios tty;
+
+    // Read in existing settings, and handle any error
+    // NOTE: This is important! POSIX states that the struct passed to tcsetattr()
+    // must have been initialized with a call to tcgetattr() overwise behaviour
+    // is undefined
+    if(tcgetattr(serial_port, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
+
+    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    //tty.c_cflag |= PARENB;  // Set parity bit, enabling parity
+
+    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    //tty.c_cflag |= CSTOPB;  // Set stop field, two stop bits used in communication
+
+    tty.c_cflag &= ~CSIZE; // Clear all the size bits, then use one of the statements below
+    //tty.c_cflag |= CS5; // 5 bits per byte
+    //tty.c_cflag |= CS6; // 6 bits per byte
+    //tty.c_cflag |= CS7; // 7 bits per byte
+    tty.c_cflag |= CS8; // 8 bits per byte (most common)
+
+    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    //tty.c_cflag |= CRTSCTS;  // Enable RTS/CTS hardware flow control
+
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+    tty.c_lflag &= ~ICANON; // diseable canonical mode
+    tty.c_lflag &= ~ECHO; // Disable echo
+    tty.c_lflag &= ~ECHOE; // Disable erasure
+    tty.c_lflag &= ~ECHONL; // Disable new-line echo
+    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    //tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT IN LINUX)
+    //tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT IN LINUX)
+
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+
+    // Set in/out baud rate to be 9600
+    cfsetspeed(&tty, B9600);
+
+    // Save tty settings, also checking for error
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    }
+
+    unsigned char msg[] = { 0, 0, 10, 50, 100, 13, 'C', 'o', 'u', 'c', 'o', 'u', ' ', 'c', 'o', 'p', 'a', 'i', 'n', '\n' };
+    write(serial_port, msg, sizeof(msg));
+    
+}
